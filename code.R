880eb0b1a86a1717c95f042ca105b6106d6c06e0
GPICCOL <gpiccoli@cct.lsu.edu>
2023-02-07 12:09:00 -0600

    more stuff

--- code.R
+++ code.R
@@ -10,20 +10,20 @@ raw_scopus %>% glimpse()
 raw_scopus %>% count(field, sort = TRUE, ) %>% tally(n)
 
 raw_classified <- read_excel("data.xlsx", sheet = "Definition classification", range = "A1:U681", na = "") %>% 
-  clean_names() %>% mutate(article_number = str_sub(string = n, start = 1, end = 6))
+  clean_names() %>% mutate(article_number = str_sub(string = id, start = 1, end = 6))
 raw_classified %>% glimpse()
 raw_classified %>% distinct(article_number) %>% tally()
 raw_classified %>% nrow()
 raw_classified %>% count(str_to_lower(explicit_definition_y_n)) %>% 
   filter(`str_to_lower(explicit_definition_y_n)` == "y") %>% pull(n)
-raw_classified %>% select(article_number, type_of_contruct) %>% filter(type_of_contruct != "Excluded") %>% 
+raw_classified %>% select(article_number, type_of_construct) %>% filter(type_of_construct != "Excluded") %>% 
   distinct(article_number) %>% tally() %>% pull(n)
 
-# constructs <- c("Organizational Capability", "IT Capability", 
-#                 "IT-enabled Capability", "Digital Capability", "Excluded")
+constructs <- c("Organizational capability", "IT capability",
+                "IT-enabled capability", "Digital capability", "Excluded")
 
-constructs <- c("Organizational capabilities", "IT capabilities", 
-                "IT-enabled capabilities", "Digital capabilities", "Excluded")
+# constructs <- c("Organizational capabilities", "IT capabilities", 
+#                 "IT-enabled capabilities", "Digital capabilities", "Excluded")
 
 df <-
   raw_scopus %>% 
@@ -48,7 +48,7 @@ df_classified <-
                                 (field == "HR" | field == "ORGANISATION STUDIES" | field == "MANAGEMENT") ~ "Management",
                                 (field == "STRATEGY" | field == "INNOVATION") ~ "Strategy and Innovation",
                                 .default = NA)) %>% 
-           mutate(construct = factor(type_of_contruct, levels = constructs, ordered = TRUE), .keep = "unused")
+           mutate(construct = factor(type_of_construct, levels = constructs, ordered = TRUE), .keep = "all")
 
 df %>% filter(is.na(discipline)) %>% count(discipline, sort = TRUE)
 df %>% count(discipline, sort = TRUE)
@@ -104,69 +104,133 @@ df_classified %>% select(year, construct, discipline) %>%
   theme(legend.position="top") +
   theme(legend.title=element_blank())
 
+#Article construct names versus our categorization
+df_mutated <- df_classified %>% 
+  mutate(construct_name_clean= str_replace_all(construct_name,  
+                                               c("capabilities"= "capability","capabilites"= "capability",
+                                                 "competencies"= "competence", "infrastructures"= "infrastructure",
+                                                 "abilities"= "ability", "analytics"= "analytic", "IT" = "InformationTechnology", 
+                                                 "IS"= "InformationSystems", "spannig" ="spanning")), 
+         construct_def_clean= str_replace_all(construct_definition,  c("capabilities"= "capability","capabilites"= "capability", 
+                                                                       "competencies"= "competence", 
+                                                                       "infrastructures"= "infrastructure",
+                                                                       "abilities"= "ability", "analytics"= "analytic", 
+                                                                       "IT" = "InformationTechnology", "IS"= 
+                                                                         "InformationSystems","spannig" ="spanning"))) %>%
+  mutate(construct_name_clean= str_replace_all(construct_name_clean, "[:digit:]", ""), 
+         construct_def_clean= str_replace_all(construct_def_clean, "[:digit:]", "")) %>% 
+  mutate(construct_name_clean= str_to_lower(construct_name_clean, locale = "en"), 
+         construct_def_clean=str_to_lower(construct_def_clean, locale = "en")) 
+
+df_classified %>% group_by(construct) %>% tally()
+df_mutated %>% group_by(construct) %>% tally()
+df_mutated %>% select(construct_name_clean) %>% filter(is.na(construct_name_clean))
+
+##Number of constructs in each classification
+df_mutated %>% filter(construct != "Excluded") %>% 
+  group_by(construct) %>% 
+  count(construct_name_clean) %>% tally()
+
+##Number of constructs in each classification per discipline
+df_mutated %>% filter(construct != "Excluded") %>% 
+  group_by(construct, discipline) %>% 
+  count(construct_name_clean) %>% tally()
+
+##Number of constructs in each classification per discipline
+df_mutated %>% filter(construct != "Excluded") %>% 
+  group_by(construct_name_clean, discipline) %>% tally() %>% arrange(desc(n)) %>% 
+  spread(key = discipline, value = n) %>% View()
+  mutate(percentage = percent(Yes / (No + Yes), accuracy = 0.1))
+
+##Number of constructs in each discipline
+  df_mutated %>% filter(construct != "Excluded") %>% 
+  group_by(discipline, construct, construct_name_clean) %>% tally() %>% arrange(desc(n), .by_group = TRUE)
+
+  df_mutated %>% filter(construct != "Excluded") %>% 
+  group_by(construct_name_clean, construct, discipline) %>% tally() %>% arrange(desc(n), .by_group = TRUE) %>% View()
 
 
 
+##Biagio's bigram analyis
+library(tidytext)
+library(yardstick)
+library(igraph)
+library(ggraph)
+
+df_mutated <- df_classified %>% 
+  mutate(construct_name_clean= str_replace_all(construct_name,  
+                                               c("capabilities"= "capability","capabilites"= "capability",
+                                                 "competencies"= "competence", "infrastructures"= "infrastructure",
+                                                 "abilities"= "ability", "analytics"= "analytic", "IT" = "InformationTechnology", 
+                                                 "IS"= "InformationSystems", "spannig" ="spanning")), 
+         construct_def_clean= str_replace_all(construct_definition,  c("capabilities"= "capability","capabilites"= "capability", 
+                                                                       "competencies"= "competence", 
+                                                                       "infrastructures"= "infrastructure",
+                                                                       "abilities"= "ability", "analytics"= "analytic", 
+                                                                       "IT" = "InformationTechnology", "IS"= 
+                                                                         "InformationSystems","spannig" ="spanning"))) %>%
+  mutate(construct_name_clean= str_replace_all(construct_name_clean, "[:digit:]", ""), 
+         construct_def_clean= str_replace_all(construct_def_clean, "[:digit:]", "")) %>% 
+  mutate(construct_name_clean= str_to_lower(construct_name_clean, locale = "en"), 
+         construct_def_clean=str_to_lower(construct_def_clean, locale = "en")) 
+
+
+#Create the bigrams
+df_bigrams <- df_mutated %>%
+  unnest_tokens(bigram, construct_name_clean, token = "ngrams", n = 2)%>%
+  filter(!is.na(bigram))
+
+df_bigrams %>%
+  count(bigram, sort = TRUE)
+
+bigrams_separated <- df_bigrams %>%
+  separate(bigram, c("word1", "word2"), sep = " ")
+
+bigrams_filtered <- bigrams_separated %>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word)
+
+# new bigram counts:
+bigram_counts <- bigrams_filtered %>% 
+  count(word1, word2, sort = TRUE)
+
+bigrams_united <- bigrams_filtered %>%
+  unite(bigram, word1, word2, sep = " ")
+
+bigram_tf_idf <- bigrams_united %>%
+  count(construct_name, bigram) %>%
+  bind_tf_idf(bigram, construct_name, n) %>%
+  arrange(desc(tf_idf))
+
+bigram_graph <- bigram_counts %>%
+  filter(n > 1) %>%
+  graph_from_data_frame()
+
+
+#Create bigram graph
+set.seed(2020)
+
+a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                 arrow = a, end_cap = circle(.1, 'inches')) +
+  geom_node_point(color = "lightblue", size = 2) +
+  geom_node_text(aes(label = name), vjust = 0.5, hjust = 0.5, check_overlap = T) +
+  theme_void()# repeat analysis on the definitions
+
+bigrams_united
+
+bigram_counts
+
+bigram_graph
+
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                 arrow = a, end_cap = circle(.1, 'inches')) +
+  geom_node_point(color = "red", size = 2) +
+  geom_node_text(aes(label = name), vjust = 0.5, hjust = 0.5, check_overlap = T) +
+  theme_void()# repeat analysis on the definitions
 
 
 
-
-
-
-###########################################
-df <- predf %>% 
-  pivot_longer(cols = c('x20131s':'x20223s'))
-
-df <-
-  df %>% 
-  mutate(name = str_remove(name, pattern = 'x')) %>% 
-  mutate(name = str_remove(name, pattern = 's')) %>% 
-  separate(col = name, into = c('year', 'semester'), sep = 4) %>% 
-  group_by(record_nbr) %>% 
-  mutate(test = ! is.na(value)) %>% 
-  mutate(cum_test = cumsum(test)) %>% 
-  mutate(max_test = max(cum_test)) %>% 
-  filter(cum_test > 0) %>% 
-  filter(! ((cum_test == max_test) & (test == FALSE)) ) %>% 
-  ungroup() %>% 
-  select(-test, -cum_test, -max_test) %>% 
-  group_by(record_nbr) %>% 
-  mutate(semester_seq = 1:length(value)) %>% 
-  pivot_wider(id_cols = c(record_nbr, orig_term, degree_term), names_from = semester_seq, values_from = value) %>% 
-  ungroup()
-
-glimpse(df)
-
-load(file="updateDF.RData")
-
-glimpse(df_test)
-df <- df_test %>% ungroup() %>% mutate(degree_year = str_sub(degree_term, 1, 4)) %>% 
-  mutate(corrected_first_semester = str_trim(`1`, side = "both"), 
-         corrected_end_major = str_trim(end_major, side = "both"))
-
-df %>% filter(degree_term != "not graduated", end_major == "ISDS") %>% 
-  count(degree_year, sort = FALSE) %>% View()
-
-df %>% filter(degree_term != "not graduated", corrected_end_major == "ISDS") %>% 
-  count(corrected_first_semester, sort = TRUE) %>% 
-  mutate(percent = n / sum(n) * 100) %>% View()
-
-df %>% filter(degree_term != "not graduated", corrected_end_major == "ISDS") %>% 
-  group_by(degree_year) %>% 
-  count(corrected_first_semester, sort = TRUE) %>% 
-  slice_max(n, n = 3) %>% View()
-
-df <- read_excel("BS ISDS Grads_2017-2021_Nanine UPDATES.xlsx", sheet = "Sheet1", range = "A1:R343", 
-                 col_types = c(rep("text", 3), "date", rep("text", 14)), na = "") %>% clean_names()
-df %>% glimpse()
-
-
-df %>% count(company, sort = TRUE, ) %>% filter(!is.na(company)) %>% writeData(wb, sheet = 1, .)
-
-df %>% count(first_company, sort = TRUE) %>% filter(!is.na(first_company)) %>% writeData(wb, sheet = 2, .)
-
-df %>% count(company, current_role, sort = TRUE) %>% filter(!is.na(company)) %>% writeData(wb, sheet = 3, .)
-
-df %>% count(first_company, first_role, sort = TRUE) %>% filter(!is.na(first_company)) %>% writeData(wb, sheet = 4, .)
-
-saveWorkbook(wb, "recrutiers.xlsx", overwrite = TRUE)
